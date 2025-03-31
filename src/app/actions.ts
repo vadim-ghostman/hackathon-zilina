c565e922a0b77a7621dd7efe3e7bd1a313a658f6
vadim-ghostman <vadimtovtin@gmail.com>
2025-03-31 23:34:05 +0200

    fixes

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,7 @@
 import webpush from "web-push";
 
 webpush.setVapidDetails(
-  process.env.NEXT_PUBLIC_EMAIL_ADDRESS,
+  process.env.NEXT_PUBLIC_EMAIL_ADDRESS!,
   process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
   process.env.VAPID_PRIVATE_KEY!,
 );
