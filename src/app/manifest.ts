3c5b3b4c470e541d28465a3279437e7a667b7479
vadim-ghostman <vadimtovtin@gmail.com>
2025-03-31 23:19:58 +0200

    added notifications skeleton

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Moje Kysuce',
+    short_name: 'KysucePWA',
+    description: 'PWA for Kysuce microregion',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+    ],
+  }
+}
